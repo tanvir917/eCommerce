d36a7858839121137d1feb43b5a3892179a070c5
tanvir <mdtanvirislams@gmail.com>
2020-07-14 22:04:18 +0600

    outputting cart items and delete items

--- store/reducers/cart.js
+++ store/reducers/cart.js
@@ -1,9 +1,8 @@
-import { ADD_TO_CART } from "../actions/cart";
+import { ADD_TO_CART, REMOVE_FROM_CART } from "../actions/cart";
 import CartItem from '../../models/cart-item';
-import { add } from "react-native-reanimated";
 
 const initialState = {
-    items: { },
+    items: {},
     totalAmount: 0
 };
 
@@ -32,6 +31,28 @@ export default (state = initialState, action) => {
                 items: { ...state.items, [addedProduct.id]: updatedOrNewCartItem },
                 totalAmount: state.totalAmount + prodPrice
             };
+        case REMOVE_FROM_CART: 
+            const selectedCartItem = state.items[action.pid];
+            const currentQty = selectedCartItem.quantity;
+            let updatedCartItems;
+            if (currentQty > 1) {
+                //need to reduce it, not erase it
+                const updatedCartItem = new CartItem(
+                    selectedCartItem.quantity - 1, 
+                    selectedCartItem.productPrice ,
+                    selectedCartItem.productTitle,
+                    selectedCartItem.sum - selectedCartItem.productPrice
+                );
+                updatedCartItems = { ...state.items, [action.pid]: updatedCartItem };
+            } else {
+                const updatedCartItems = { ...state.items };
+                delete updatedCartItems[action.pid];
+            }
+            return {
+                ...state,
+                items: updatedCartItems,
+                totalAmount: state.totalAmount - selectedCartItem.productPrice
+            }
     }
     return state;
 }
