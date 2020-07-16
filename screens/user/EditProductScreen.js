59227efb1de9c8c9f7e90737b987a4da87b7a8c3
TANVIR ISLAM <mdtanvirislams@gmail.com>
2020-07-17 02:13:37 +0600

    dispatching actions for creating and updating

--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -2,13 +2,15 @@ import React, { useState, useEffect, useCallback  } from 'react';
 import { View, ScrollView, Text, TextInput, StyleSheet, Platform } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import HeaderButton from '../../components/UI/HeaderButton';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import * as productsActions from '../../store/actions/products';
 
 const EditProductScreen = props => {
     const prodId = props.navigation.getParam('productId');
     const editedProduct = useSelector(state => 
         state.products.userProducts.find(prod => prod.id === prodId)
     );
+    const dispatch = useDispatch();
 
     const [title, setTitle] = useState(editedProduct ? editedProduct.title: '');
     const [imageUrl, setImageUrl] = useState(editedProduct ? editedProduct.imageUrl: '');
@@ -16,8 +18,17 @@ const EditProductScreen = props => {
     const [description, setDescription] = useState(editedProduct ? editedProduct.description :'');
 
     const submitHandler = useCallback(() => {
-        console.log('Submitting!');
-    }, []);
+        //console.log('Submitting!');
+        if (editedProduct) {
+            dispatch(
+                productsActions.updateProduct(prodId, title, description, imageUrl)
+            );
+        } else {
+            dispatch(
+                productsActions.createProduct(title, description, imageUrl, +price)
+            );
+        }
+    }, [dispatch, prodId, title, description, imageUrl, price]);
 
     useEffect(() => {
         props.navigation.setParams({ 'submit': submitHandler })
