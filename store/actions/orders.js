89aefe3afa5e04fcabaa536574d07bab178051b2
tanvir <mdtanvirislams@gmail.com>
2020-07-28 02:07:45 +0600

    fetching stored orders with spinner

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -1,4 +1,37 @@
+import Order from '../../models/order';
+
 export const ADD_ORDER = 'AND_ORDER';
+export const SET_ORDERS = 'SET_ORDERS';
+
+export const fetchOrders = () => {
+    return async dispatch => {
+        try {
+            const response = await fetch('https://ecommerce-fed7f.firebaseio.com/orders/u1.json') ;
+
+            if (!response.ok) {
+                throw new Error('Something went wrong!');
+            }
+
+            const resData = await response.json();
+            const loadedOrders = [];
+
+            for (const key in resData) {
+                loadedOrders.push(
+                    new Order(
+                        key, 
+                        resData[key].cartItems, 
+                        resData[key].totalAmount, 
+                        new Date(resData[key].date)
+                    )
+                );
+            }
+
+            dispatch({ type: SET_ORDERS, orders: loadedOrders });//dispatching an ation
+        } catch (err) {
+            throw err;
+        }
+    }
+};
 
 //action creator
 export const addOrder = (cartItems, totalAmount) => {
