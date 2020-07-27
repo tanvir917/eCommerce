a654f167763b0ca0289f5d128363f441865dcd27
tanvir <mdtanvirislams@gmail.com>
2020-07-28 01:04:23 +0600

    storing orders

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -2,8 +2,35 @@ export const ADD_ORDER = 'AND_ORDER';
 
 //action creator
 export const addOrder = (cartItems, totalAmount) => {
-    return {
-        type: ADD_ORDER, 
-        orderData: { items: cartItems, amount: totalAmount }
-    };
+    return async dispatch => {
+        const date = new Date();
+        //send a req to store product on server
+        const response = await fetch('https://ecommerce-fed7f.firebaseio.com/orders/u1.json', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                cartItems,
+                totalAmount,
+                date: date.toISOString()
+            })
+        });
+
+        if (!response.ok) {
+            throw new Error('Something went wrong!');
+        }
+
+        const resData = await response.json();
+
+        dispatch ({
+            type: ADD_ORDER, 
+            orderData: { 
+                id: resData.name, 
+                items: cartItems, 
+                amount: totalAmount, 
+                date: date 
+            }
+        });
+    }
 };
