ac95762eb42ddd6975544481d5df2a2544131cf4
tanvir <mdtanvirislams@gmail.com>
2020-07-29 00:45:26 +0600

    loging users in

--- store/actions/auth.js
+++ store/actions/auth.js
@@ -1,5 +1,5 @@
-
 export const SIGNUP = 'SIGNUP';
+export const LOGIN = 'LOGIN';
 
 export const signup = (email, password) => {
     return async dispatch => {
@@ -28,9 +28,43 @@ export const signup = (email, password) => {
     
             dispatch({ type: SIGNUP });
         } catch (err) {
-            console.log('printing error: ');
+            //console.log('printing error: ');
             console.log(err);
-            console.log('finishing error');
+            //console.log('finishing error');
+            throw err;
+        }
+    }
+}
+
+export const login = (email, password) => {
+    return async dispatch => {
+        try {
+            const response = await fetch(
+                'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAPNzsTfArr1GyL-g3hIDT5hEsIPsh8T7k', 
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        email: email,
+                        password: password,
+                        returnSecureToken: true
+                    })
+                }
+            );
+    
+            if (!response.ok) {
+                throw new Error('Something went wrong 1!');
+            }
+    
+            const resData = await response.json();
+            console.log(resData);
+            dispatch({ type: LOGIN });
+        } catch (err) {
+            //console.log('printing error: ');
+            console.log(err);
+            //console.log('finishing error');
             throw err;
         }
     }
