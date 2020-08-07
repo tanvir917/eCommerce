e582cc4eb69e57edaee23e6c8e077504908f236f
tanvir <mdtanvirislams@gmail.com>
2020-08-07 18:53:38 +0600

    implementing search options

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState, useCallback } from 'react';
 import { View, Text, FlatList, Button, Platform, ActivityIndicator, StyleSheet } from 'react-native';
+import { SearchBar } from 'react-native-elements';
+//import SearchBar from 'react-native-search-bar';
 import { useSelector, useDispatch } from 'react-redux';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
@@ -12,9 +14,23 @@ import Colors from '../../constants/Colors';
 const ProductsOverviewScreen = props => {
     const [isLoading, setIsLoading] = useState(false);
     const [isRefreshing, setIsRefreshing] = useState(false);
+    const [value, setValue] = useState();
+    
     const [error, setError] = useState();
     const products = useSelector(state => state.products.availableProducts);
+    const [arrayholder, setArrayholder] = useState(products);
     const dispatch = useDispatch();
+    
+    const productF = products;
+    // console.log(arrayholder);
+    // const arr = productF.filter(item => {
+    //     const itemData = item.title.toUpperCase();
+    //     console.log('title: ', itemData);
+    //     return itemData;
+    // });
+    // setArrayholder(arr);
+    // console.log('arr: ',arr);
+
 
     const loadProducts = useCallback(async () => {
         console.log('Load Products');
@@ -53,6 +69,42 @@ const ProductsOverviewScreen = props => {
         });
     };
 
+    const searchFilterFunction = text => {
+        // setState({
+        //   value: text,
+        // });
+        setValue(text);
+    
+        const newData = productF.filter(item => {
+          const itemData = item.title.toUpperCase();
+          const textData = text.toUpperCase();
+    
+          return itemData.indexOf(textData) > -1;
+        });
+        // setState({
+        //   data: newData,
+        // });
+        //arrayholder = newData;
+        setArrayholder(newData);
+        // console.log('arrayholder:');
+        // console.log(arrayholder);
+      };
+
+    // const renderHeader = () => {
+    //     return (
+    //       <SearchBar
+    //         focus
+    //         placeholder="Type Here..."
+    //         lightTheme
+    //         round
+    //         onChangeText={text => searchFilterFunction(text)}
+    //         //onSearchButtonPress={text => searchFilterFunction(text)}
+    //         autoCorrect={false}
+    //         value={value}
+    //       />
+    //     );
+    //   };
+
     if (error) {
         return (
             <View style={styles.centered}>
@@ -84,36 +136,51 @@ const ProductsOverviewScreen = props => {
 
     
     return (
-        <FlatList 
-            onRefresh={loadProducts}
-            refreshing={isRefreshing}
-            data={products} 
-            keyExtractor={item => item.id} 
-            renderItem={itemData => <ProductItem 
-                image={itemData.item.imageUrl}
-                title={itemData.item.title}
-                price={itemData.item.price}
-                onSelect={() => {
-                    selectItemHandler(itemData.item.id, itemData.item.title)
-                }}  
-            >
-            <Button 
-                color={Colors.primary} 
-                title="View Details" 
-                onPress={() => {
-                    selectItemHandler(itemData.item.id, itemData.item.title)
-                }}    
+        <View style={{flex: 1}}>
+            <SearchBar
+                focus
+                placeholder="Type Here..."
+                lightTheme
+                round
+                onChangeText={text => searchFilterFunction(text)}
+                //onSearchButtonPress={text => searchFilterFunction(text)}
+                autoCorrect={false}
+                value={value}
             />
-            <Button 
-                color={Colors.primary} 
-                title="To Cart" 
-                onPress={() => {
-                    dispatch(cartActions.addToCart(itemData.item));
-                }}     
+            <FlatList 
+                onRefresh={loadProducts}
+                refreshing={isRefreshing}
+                //arrayholder.length === 0 ? products: 
+                data={arrayholder.length === 0 ? products : arrayholder} 
+                keyExtractor={item => item.id} 
+                renderItem={itemData => <ProductItem 
+                    image={itemData.item.imageUrl}
+                    title={itemData.item.title}
+                    price={itemData.item.price}
+                    onSelect={() => {
+                        selectItemHandler(itemData.item.id, itemData.item.title)
+                    }}  
+                >
+                <Button 
+                    color={Colors.primary} 
+                    title="View Details" 
+                    onPress={() => {
+                        selectItemHandler(itemData.item.id, itemData.item.title)
+                    }}    
+                />
+                <Button 
+                    color={Colors.primary} 
+                    title="To Cart" 
+                    onPress={() => {
+                        dispatch(cartActions.addToCart(itemData.item));
+                    }}     
+                />
+                </ProductItem>
+                } 
+                //ListHeaderComponent={renderHeader}
             />
-            </ProductItem>
-            } 
-        />
+            
+        </View>
     );
 };
 
